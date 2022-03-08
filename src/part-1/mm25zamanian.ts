9e5c05f4fab7544ff5321fbb6e52d2978ed2f16d
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-03-09 01:06:12 +0330

    Exercises <part-1> by @MM25Zamanian

--- src/part-1/mm25zamanian.ts
+++ src/part-1/mm25zamanian.ts
@@ -1,7 +1,19 @@
 /* Part One - I'm MohammadMahdi Zamanian <mm25zamanain@gmail.com> @MM25Zamanian */
 
+/**
+ * Function for multiplying two numbers together
+ *
+ * @example
+ * const number = myFunction(10, 5);
+ * console.log(number); // 50
+ * console.log(typeof number); // number
+ */
 function myFunction(number1:number, number2:number):number {
   return number1 * number2;
 }
 
-console.log(myFunction(10, 5));
+/* Simple example */
+console.log(myFunction(5, 4)); // 20
+
+/* A complex example */
+console.log(myFunction(5, myFunction(4, 3))); // 60
