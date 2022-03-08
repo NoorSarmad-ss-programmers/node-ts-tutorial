4b4271ab90caa249437ee1218e06d4ef6da720d6
MohammadMahdi Zamanian <mm25zamanian@gmail.com>
2022-03-09 00:21:33 +0330

    Build <part-1> file from <@MM25Zamanian>

---
+++ src/part-1/mm25zamanian.ts
@@ -0,0 +1,7 @@
+/* Part One - I'm MohammadMahdi Zamanian <mm25zamanain@gmail.com> @MM25Zamanian */
+
+function myFunction(number1:number, number2:number):number {
+  return number1 * number2;
+}
+
+console.log(myFunction(10, 5));
